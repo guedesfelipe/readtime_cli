522a325414fe3ebdb32188d6385905f547b2c695
Felipe Guedes <contatofelipeguedes@gmail.com>
2022-05-16 20:12:53 -0300

    ✨ Add help and wpm option

--- readtime_cli/main.py
+++ readtime_cli/main.py
@@ -13,15 +13,24 @@ def version():
 
 
 @app.command()
-def text(file: typer.FileText = typer.Argument(...)):
-    typer.echo(readtime.of_text(''.join(file.readlines())))
+def text(
+    file: typer.FileText = typer.Argument(..., help='PATH'),
+    wpm: int = typer.Option(265, help='Word Per Minute'),
+):
+    typer.echo(readtime.of_text(''.join(file.readlines()), wpm=wpm))
 
 
 @app.command()
-def md(file: typer.FileText = typer.Argument(...)):
-    typer.echo(readtime.of_markdown(''.join(file.readlines())))
+def md(
+    file: typer.FileText = typer.Argument(..., help='PATH'),
+    wpm: int = typer.Option(265, help='Word Per Minute'),
+):
+    typer.echo(readtime.of_markdown(''.join(file.readlines()), wpm=wpm))
 
 
 @app.command()
-def html(file: typer.FileText = typer.Argument(...)):
-    typer.echo(readtime.of_html(''.join(file.readlines())))
+def html(
+    file: typer.FileText = typer.Argument(..., help='PATH'),
+    wpm: int = typer.Option(265, help='Word Per Minute'),
+):
+    typer.echo(readtime.of_html(''.join(file.readlines()), wpm=wpm))
